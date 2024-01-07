0c7d3f97c79d8814190d3888a30a129cbe78f7d6
dina veladika <121397277+dinaveladika@users.noreply.github.com>
2024-01-07 12:45:58 +0300

    Update car_detection.py

--- car_detection.py
+++ car_detection.py
@@ -7,7 +7,7 @@ model = project.version(1).model
 print(model.predict("trafik-istanbul.jpg", confidence=40, overlap=30).json())
 
 # visualize your prediction
-# model.predict("your_image.jpg", confidence=40, overlap=30).save("prediction.jpg")
+# model.predict("trafik-istanbul.jpg", confidence=40, overlap=30).save("prediction.jpg")
 
 # infer on an image hosted elsewhere
 # print(model.predict("URL_OF_YOUR_IMAGE", hosted=True, confidence=40, overlap=30).json())
